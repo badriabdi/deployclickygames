42283ad1f5f3fe4b7b3ac9ef92435865fd0016a8
badri m abdi <badriabdi877@gmail.com>
2019-04-28 01:40:06 -0600

    Updates

---
+++ precache-manifest.aff57bc6212fbb1a5bcdd0ab3390cfcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e67ce4701c143c6f5e8e1ea3d805bb",
+    "url": "/deployclickygames/index.html"
+  },
+  {
+    "revision": "b6c38e1e4f98351b77ce",
+    "url": "/deployclickygames/static/css/main.b7d3e95d.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/deployclickygames/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b6c38e1e4f98351b77ce",
+    "url": "/deployclickygames/static/js/main.27bd3d49.chunk.js"
+  },
+  {
+    "revision": "3eaff6555960fd099af7",
+    "url": "/deployclickygames/static/js/runtime~main.83353748.js"
+  }
+]);
